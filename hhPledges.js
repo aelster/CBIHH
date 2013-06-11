710d5a9219693fc8cc9baeacb3387fbbc76b7c03
Andy Elster <andy@elsternet.com>
2013-06-10 22:48:06 -0700

    Working pretty good

--- hhPledges.js
+++ hhPledges.js
@@ -5,6 +5,12 @@ function addAction(arg) {
 	form1.submit();
 }
 
+function clearSpiritOther() {
+	var e = document.getElementById('spiritOther');
+	e.value = '';
+	e.focus();
+}
+
 function makeActive(area) {
 	//background-color: #ADB96E;
 	var e, ok;
@@ -22,6 +28,29 @@ function makeActive(area) {
 			}
 		}
 		e = document.getElementById('pledgeNow');
+		if( ok ) {
+			e.className = 'buttonOk';
+			e.disabled = false;
+		} else {
+			e.className = 'buttonNotOk';
+			e.disabled = true;
+		}
+		
+	} else if( area == 'spirit' ) {
+		e = document.getElementsByName('spirit');
+		for( var i=0; i<e.length; i++ ) {
+			if( e[i].id == 'other' ) {
+				var f = document.getElementById('spiritOther');
+				if( e[i].checked ) {
+					if( f.value !== '' ) ok = 1;
+				} else {
+					f.value = 'Please enter description';
+				}
+			} else {
+				if( e[i].checked ) ok = 1;
+			}
+		}
+		e = document.getElementById('spiritNow');
 		if( ok ) {
 			e.className = 'buttonOk';
 			e.disabled = false;
