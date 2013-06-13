372c9b3fb3a9c1d3e48e7b8f8406db2347a7c306
Andy Elster <andy@elsternet.com>
2013-06-12 23:37:53 -0700

    Update for tonight. To do stuff:
    o Have both Financial/Spiritual go through same checkout page
    o Change/Try to modify all html to PHP pages
    o continue mail integration
    o establish MySQL database/integration
    o need a MySQL edit page/Admin page to correct errors

--- hhPledges.js
+++ hhPledges.js
@@ -11,6 +11,10 @@ function clearSpiritOther() {
 	e.focus();
 }
 
+function goToURL(page) {
+	window.location.href = page;
+}
+
 function makeActive(area) {
 	//background-color: #ADB96E;
 	var e, ok;
@@ -58,5 +62,39 @@ function makeActive(area) {
 			e.className = 'buttonNotOk';
 			e.disabled = true;
 		}
+		
+	} else if( area == 'paynow' ) {
+		e = document.getElementsByName('paynow');
+		var num_filled = 0;
+		var radio = 0;
+		for( var i=0; i<e.length; i++ ) {
+			if( e[i].type == 'text' ) {
+				if( e[i].value !== '' ) num_filled++;
+			} else if( e[i].type == 'radio' ) {
+				if( e[i].checked ) radio++;
+			}
+		}
+		
+		ok = ( num_filled == 4 && radio > 0 );
+		e = document.getElementById('paynow');
+		if( ok ) {
+			e.className = 'buttonOk';
+			e.disabled = false;
+		} else {
+			e.className = 'buttonNotOk';
+			e.disabled = true;
+		}
 	}
+}
+
+function payNow() {
+	var items = new Array();
+	var keys = new Array( 'lastName', 'firstName', 'phone', 'email' );
+	var e;
+	for( var i=0; i<keys.length; i++ ) {
+		e = document.getElementById(keys[i]);
+		items[i] = keys[i] + '=' + e.value;
+	}
+	e = document.getElementById('fields');
+	e.value = items.join('|');
 }
--- index.php
+++ index.php
@@ -1,13 +1,11 @@
+<?php
+require_once 'lib/swift_required.php';
+?>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>CBI HH Pledges</title>
-
-<script type="text/javascript">
-function goToURL(page) {
-	window.location.href = page;
-}
-</script>
+<script type="text/javascript" src="hhPledges.js"></script>
 </head>
 <body>
 <?php
@@ -28,6 +26,27 @@ if( $action == "pledge" ) {
 
 } elseif( $action == "spiritual" ) {
 	$url = "http://" . $_SERVER['SERVER_NAME'] . preg_replace( '/index.php/', 'spiritual.html', $_SERVER['SCRIPT_NAME'] );
+	echo "<script type=\"text/javascript\">goToURL('$url');</script>";
+
+} elseif( $action == "paynow" ) {
+	$keys = array_keys( $_POST );
+	sort( $keys );
+	foreach( $keys as $key ) {
+//		printf( "_POST[%s] = %s<br>", $key, $_POST[$key] );
+	}
+	$tmp = preg_split( '/\|/', $_POST['fields'] );
+	foreach( $tmp as $xx ) {
+		list( $key, $val ) = preg_split( '/=/', $xx );
+		$$key = $val;
+	}
+	$pmt_type = $_POST['paynow'];
+	$message = Swift_Message::newInstance()
+	->setSubject('CBI Financial Pledge Confirmation' )
+	->setFrom(array('cbi18@cbi18.org' => 'CBI'))
+	->setTo(array( $email => "$firstName $lastName" ) )
+	->setCC(array( 'beth@elsternet.com' => 'The Pres' ) )
+	;
+	$url = "http://" . $_SERVER['SERVER_NAME'] . preg_replace( '/index.php/', 'pledge.html', $_SERVER['SCRIPT_NAME'] );
 	echo "<script type=\"text/javascript\">goToURL('$url');</script>";
 
 } else {
