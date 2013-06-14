3716dc5a7cc1e405c6fadc4235b4ea304cc0f711
Andy Elster <andy@elsternet.com>
2013-06-14 12:17:59 -0700

    Starting to link with MySQL, heading to PA for the weekend

--- tbd.php
+++ tbd.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/index.dwt" codeOutsideHTMLIsLocked="false" -->
 <?php
+require_once( 'SiteLoader.php' );
+
+SiteLoad( 'CommonV2' );
+
+include( 'local_cbi.php' );
+//$gDb = OpenDb();                # Open the MySQL database
+
 global $gAction;
 global $gFrom;
 
